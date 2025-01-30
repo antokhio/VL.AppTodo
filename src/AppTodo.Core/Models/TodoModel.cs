a08a602e05c6c3398517c40fb291f34771c3ec1d
antokhio <antokhio@gmail.com>
2025-01-30 19:02:49 +0300

    chapter-1

---
+++ src/AppTodo.Core/Models/TodoModel.cs
@@ -0,0 +1,16 @@
+﻿namespace AppTodo.Core.Models;
+public record TodoModel
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public bool IsCompleted { get; set; }
+
+    //public Spread<SubtaskModel> Subtasks { get; set; }
+
+    public TodoModel(string title)
+    {
+        Id = Guid.NewGuid();
+        Title = title;
+        IsCompleted = false;
+    }
+}
---
+++ src/AppTodo.Core/Models/TodosModel.cs
@@ -0,0 +1,13 @@
+﻿using VL.Lib.Collections;
+
+namespace AppTodo.Core.Models;
+
+public record TodosModel
+{
+    public Spread<TodoModel> Todos { get; set; }
+
+    public TodosModel()
+    {
+        Todos = Spread<TodoModel>.Empty;
+    }
+}
